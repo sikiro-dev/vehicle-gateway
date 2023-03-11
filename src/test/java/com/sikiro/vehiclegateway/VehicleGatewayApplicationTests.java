2adc536a3b344afd92b9a66f90281ac15f7ee9f2
sikiro-dev <dellosomaurizio@gmail.com>
2023-03-11 10:35:02 +0100

    first commit

---
+++ src/test/java/com/sikiro/vehiclegateway/VehicleGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sikiro.vehiclegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

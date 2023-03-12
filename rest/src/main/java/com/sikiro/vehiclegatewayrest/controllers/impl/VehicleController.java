7130127462333c43d73c9b33f2345d1432ad6431
sikiro-dev <dellosomaurizio@gmail.com>
2023-03-12 12:29:45 +0100

    FIX

--- rest/src/main/java/com/sikiro/vehiclegatewayrest/controllers/impl/VehicleController.java
+++ rest/src/main/java/com/sikiro/vehiclegatewayrest/controllers/impl/VehicleController.java
@@ -4,6 +4,7 @@ import com.sikiro.vehiclegateway.models.Status;
 import com.sikiro.vehiclegateway.models.Vehicle;
 import com.sikiro.vehiclegatewayrest.controllers.Controller;
 import com.sikiro.vehiclegatewayrest.services.VehicleService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
+@Slf4j
 @RestController
 @RequestMapping("api/vehicle")
 public class VehicleController extends Controller {
@@ -25,7 +27,9 @@ public class VehicleController extends Controller {
 
     @GetMapping("{id}")
     public Mono<ResponseEntity<Vehicle>> getVehicle(@PathVariable String id) {
+        log.info("Request update for: {}", id);
         vehicleService.getUpdate(id);
+        log.info("Collecting update for: {}", id);
         return vehicleService.getVehicle(id)
                 .map(ResponseEntity::ok)
                 .timeout(Duration.ofMillis(3000), Mono.just(ResponseEntity.notFound().build()));
@@ -33,18 +37,21 @@ public class VehicleController extends Controller {
 
     @GetMapping("{id}/frequency/{frequency}")
     public ResponseEntity<Void> sendUpdateFrequency(@PathVariable String id, @PathVariable int frequency) {
+        log.info("Request update frequency for: {} with frequency: {}", id, frequency);
         vehicleService.sendUpdateFrequency(id, frequency);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("{id}/command/{command}")
     public ResponseEntity<Void> sendCommand(@PathVariable String id, @PathVariable String command) {
+        log.info("Request command for: {} with command: {}", id, command);
         vehicleService.sendCommand(id, Status.fromString(command));
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("{id}/disconnect")
     public ResponseEntity<Void> disconnect(@PathVariable String id) {
+        log.info("Request disconnect for: {}", id);
         vehicleService.disconnect(id);
         return ResponseEntity.ok().build();
     }
--- tcp/src/main/java/com/sikiro/vehiclegatewaytcp/configurations/RedisConfigurations.java
+++ tcp/src/main/java/com/sikiro/vehiclegatewaytcp/configurations/RedisConfigurations.java
@@ -2,10 +2,8 @@ package com.sikiro.vehiclegatewaytcp.configurations;
 
 import com.sikiro.vehiclegateway.models.Vehicle;
 import com.sikiro.vehiclegateway.models.messages.ServerMessage;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
@@ -16,45 +14,22 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 @Configuration
 public class RedisConfigurations {
 
-    @Value("${spring.data.redis.writer.host}")
-    private String writerHost;
-
-    @Value("${spring.data.redis.writer.port}")
-    private int writerPort;
-
-    @Value("${spring.data.redis.reader.host}")
-    private String readerHost;
-
-    @Value("${spring.data.redis.reader.port}")
-    private int readerPort;
-
     @Bean
-    @Primary
-    public LettuceConnectionFactory lettuceConnectionFactoryWriter() {
-        return new LettuceConnectionFactory(writerHost, writerPort);
-    }
-
-    @Bean
-    public LettuceConnectionFactory lettuceConnectionFactoryReader() {
-        return new LettuceConnectionFactory(readerHost, readerPort);
-    }
-
-    @Bean
-    public ReactiveRedisOperations<String, ServerMessage> messageTemplate(LettuceConnectionFactory lettuceConnectionFactoryReader) {
+    public ReactiveRedisOperations<String, ServerMessage> messageTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
         RedisSerializer<ServerMessage> valueSerializer = new Jackson2JsonRedisSerializer<>(ServerMessage.class);
         RedisSerializationContext<String, ServerMessage> serializationContext = RedisSerializationContext.<String, ServerMessage>newSerializationContext(RedisSerializer.string())
                 .value(valueSerializer)
                 .build();
-        return new ReactiveRedisTemplate<>(lettuceConnectionFactoryReader, serializationContext);
+        return new ReactiveRedisTemplate<>(lettuceConnectionFactory, serializationContext);
     }
 
     @Bean
-    public ReactiveRedisOperations<String, Vehicle> vehicleTemplate(LettuceConnectionFactory lettuceConnectionFactoryWriter) {
+    public ReactiveRedisOperations<String, Vehicle> vehicleTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
         RedisSerializer<Vehicle> valueSerializer = new Jackson2JsonRedisSerializer<>(Vehicle.class);
         RedisSerializationContext<String, Vehicle> serializationContext = RedisSerializationContext.<String, Vehicle>newSerializationContext(RedisSerializer.string())
                 .value(valueSerializer)
                 .build();
-        return new ReactiveRedisTemplate<>(lettuceConnectionFactoryWriter, serializationContext);
+        return new ReactiveRedisTemplate<>(lettuceConnectionFactory, serializationContext);
     }
 
 }
